f1cf47a70cc1da58395f1a464b8764c67c0221f3
miham <miham@marand.si>
2022-01-10 15:33:07 +0100

    small update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		disableStaticImages: false,
 		domains: ['s3.eu-central-1.amazonaws.com', 's3.eu-west-1.amazonaws.com', 'bayut-production.s3.eu-central-1.amazonaws.com'],
 	},
 };
