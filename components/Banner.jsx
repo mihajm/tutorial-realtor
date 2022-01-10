276c39204cd63bbfdf305314b7c703daab516af5
miham <miham@marand.si>
2022-01-10 15:43:59 +0100

    final import fix for deployment

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,6 +1,6 @@
-import Image from 'next/Image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react';
 import {Link} from 'next/link';
+import {Image} from 'next/Image';
 
 export const Banner = ({purpose, imageUrl, title1, title2, desc1, desc2, linkName, buttonText}) => (
 	<Flex flexWrap="warp" justifyContent="center" alignItems="center" m="10">
